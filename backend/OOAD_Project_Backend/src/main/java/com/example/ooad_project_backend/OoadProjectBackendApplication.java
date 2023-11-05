24bb5aca94cc8a538479fed86bc230e19f20f079
susjunyou <104712257+susjunyou@users.noreply.github.com>
2023-11-05 16:08:04 +0800

    Add files via upload

---
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/OoadProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ooad_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OoadProjectBackendApplication.class, args);
+    }
+
+}
