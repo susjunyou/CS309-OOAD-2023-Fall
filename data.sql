42c69e451e4f2cb1c13e0188091b813760c9b168
susjunyou <104712257+susjunyou@users.noreply.github.com>
2023-11-21 22:22:48 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -9,10 +9,10 @@ insert into admin (id, name, account, password, email, phone_number, sex)
 values (20, 'admin', '123', '123456', '123456@qq.com', '13636364747', true);
 
 insert into admin (id, name, account, password, email, phone_number, sex)
-values (20, 'admin2', '124', '123456', '123456@qq.com', '13636364747', true);
+values (21, 'admin2', '124', '123456', '123457@qq.com', '13636364747', true);
 
 insert into admin (id, name, account, password, email, phone_number, sex)
-values (20, 'admin3', '125', '123456', '123456@qq.com', '13636364747', true);
+values (22, 'admin3', '125', '123456', '123458@qq.com', '13636364747', true);
 
 
 --! student data
@@ -23,20 +23,20 @@ values (1, 'student1', '123', '123456', '123456@qq.com', 'self_Introduction', '1
 
 insert into student (id, name, account, password, email, self_Introduction, phone_number, sex, major, level, isSA,
                      department)
-values (1, 'student2', '124', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'cs', '1', false,
+values (2, 'student2', '124', '123456', '123457@qq.com', 'self_Introduction', '13636364747', true, 'cs', '1', false,
         'COMPUTER_SCIENCE');
 
 insert into student (id, name, account, password, email, self_Introduction, phone_number, sex, major, level, isSA,
                      department)
-values (1, 'student3', '125', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'cs', '1', true,
+values (3, 'student3', '125', '123456', '123458@qq.com', 'self_Introduction', '13636364747', true, 'cs', '1', true,
         'COMPUTER_SCIENCE');
 
 --! teacher data
 insert into teacher (id, name, account, password, email, self_Introduction, phone_number, sex, department, tenure)
-values (1, 'teacher1', '123', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
+values (11, 'teacher1', '123', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
 
 insert into teacher (id, name, account, password, email, self_Introduction, phone_number, sex, department, tenure)
-values (1, 'teacher2', '124', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
+values (22, 'teacher2', '124', '123456', '123457@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
 
 insert into teacher (id, name, account, password, email, self_Introduction, phone_number, sex, department, tenure)
-values (1, 'teacher3', '125', '123456', '123456@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
+values (33, 'teacher3', '125', '123456', '123458@qq.com', 'self_Introduction', '13636364747', true, 'COMPUTER_SCIENCE', 'PROFESSOR');
