209e6d0f8fd60ab2a89274351a31952c345f1772
Zjc20021104 <12110320@mail.sustech.edu.cn>
2023-12-02 21:25:44 +0800

    增加修改个人信息

--- frontend/vue-project-introduction-master/src/views/StudentHomePage.vue
+++ frontend/vue-project-introduction-master/src/views/StudentHomePage.vue
@@ -2,32 +2,71 @@
 <template>
 
   <div id="app">
-      <el-row class="header-bar" background-color="#545c64" text-color="#fff">
-        <el-col :span="14">
-          <h1 class="header-title">Project Helper</h1>
-        </el-col>
-        <el-col :span="10">
-          <el-button type="text" v-popover:profilePopover class="profile-button">
-            <i class="el-icon-user"></i> 个人资料
-          </el-button>
-          <el-popover ref="profilePopover" placement="bottom" width="200" trigger="click">
-            <!-- popover内容 -->
-            <div class="user-profile">
-              <img src="../assets/人脸.png" alt="个人信息" class="avatar">
-              <h3>姓名：{{ this.name }}</h3>
-              <p>学号：{{ this.id }}</p>
-              <p>邮箱：{{ this.email }}</p>
-              <p>专业：{{ this.major }}</p>
-              <el-menu>
-                <el-menu-item index="1" @click="go('updatePassword')">修改密码</el-menu-item>
-                <el-menu-item index="2" @click="go('PersonInformation')">修改个人信息</el-menu-item>
-              </el-menu>
-            </div>
-          </el-popover>
+    <el-row class="header-bar" background-color="#545c64" text-color="#fff">
+      <el-col :span="14">
+        <h1 class="header-title">Project Helper</h1>
+      </el-col>
+      <el-col :span="10">
+        <el-button type="text" v-popover:profilePopover class="profile-button">
+          <i class="el-icon-user"></i> 个人资料
+        </el-button>
+        <el-popover ref="profilePopover" placement="bottom" width="200" trigger="click">
+          <!-- popover内容 -->
+          <div class="user-profile">
+            <img src="../assets/人脸.png" alt="个人信息" class="avatar">
+            <h3>姓名：{{ this.name }}</h3>
+            <p>学号：{{ this.id }}</p>
+            <p>邮箱：{{ this.email }}</p>
+            <p>专业：{{ this.major }}</p>
+            <el-menu>
+              <el-menu-item index="1" @click="go('updatePassword')">修改密码</el-menu-item>
+              <el-menu-item index="2" @click="update">修改个人信息</el-menu-item>
+            </el-menu>
+          </div>
+        </el-popover>
 
-        </el-col>
-      </el-row>
+      </el-col>
+    </el-row>
 
+
+    <el-dialog
+        :visible.sync="dialogVisible"
+        title="Add A New Record"
+        width="40%"
+
+    >
+      <el-form
+          ref="editForm"
+          :model="edit"
+          :rules="rules"
+          label-width="auto"
+          label-position="right"
+          size="default"
+
+
+      >
+        <el-form-item label="e_id" prop="e_id">
+          <el-input v-model="edit.e_id"/>
+        </el-form-item>
+
+        <el-form-item label="e_email" prop="e_email">
+          <el-input v-model="edit.e_email"/>
+        </el-form-item>
+
+        <el-form-item label="e_phoneNumber" prop="e_phoneNumber">
+          <el-input v-model="edit.e_phoneNumber"/>
+        </el-form-item>
+
+        <el-form-item label="e_selfIntroduction" prop="e_selfIntroduction">
+          <el-input v-model="edit.e_selfIntroduction"/>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="commitUpdate()">Submit</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-dialog>
 
     <el-row class="content-wrapper">
       <!-- 课程卡片列表 -->
@@ -53,7 +92,51 @@
 export default {
   name: 'StudentHomepage',
   data() {
+    const e_idValidator = (rule, value, callback) => {///^[A-Za-z0-9]+/
+      const re = /^[0-9]/;
+      if (!value) {
+        return callback(new Error('Please input id'));
+      }
+      if (!re.test(value)) {
+        return callback(new Error('Invalid id, should only contain numbers'));
+      }
+      callback();
+    };
+
+    const e_phoneNumberValidator = (rule, value, callback) => {///^[A-Za-z0-9]+/
+      const re = /^[0-9]+/;//(?=.*[0-9])(?=.*[a-zA-Z])
+      if (!value) {
+        return callback(new Error('Please input phoneNumber'));
+      }
+      if (!re.test(value)) {
+        return callback(new Error('Invalid phoneNumber, should only contain numbers'));
+      }
+      callback();
+    };
+
     return {
+      rules: {
+        //
+        e_id: [
+          {validator: e_idValidator, trigger: 'blur'},
+          {required: true, message: 'Please input the id', trigger: 'blur'},
+        ],
+        email: [
+          //{validator: emailValidator(), trigger: 'blur'},
+          {required: true, message: 'Please input the id', trigger: 'blur'},
+        ],
+        e_phoneNumber: [
+          {validator: e_phoneNumberValidator, trigger: 'blur'},
+          {required: true, message: 'Please input the id', trigger: 'blur'},
+        ],
+      },
+      dialogVisible: false,
+      edit: {
+        e_id: "",
+        e_email:"",
+        e_phoneNumber:"",
+        e_selfIntroduction:""
+      },
       courses: [],
       ddls: [
         // ...其他DDL
@@ -70,6 +153,46 @@ export default {
     };
   },
   methods: {
+    update(){
+      this.dialogVisible=true;
+      this.edit.e_id = this.id;
+      this.edit.e_email = this.email;
+      this.edit.e_phoneNumber = localStorage.getItem('phoneNumber');
+      this.edit.e_selfIntroduction = localStorage.getItem('selfIntroduction');
+
+    },
+    commitUpdate(){
+      this.id = this.edit.e_id;
+      this.email = this.edit.e_email;
+      localStorage.setItem('id',this.edit.e_id);
+      localStorage.setItem('email',this.edit.e_email);
+      localStorage.setItem('phoneNumber',this.edit.e_phoneNumber);
+      localStorage.setItem('selfIntroduction',this.edit.e_selfIntroduction);
+      this.dialogVisible = false;
+      this.$axios.get('/student/updateStudentDetails',{
+        params: {
+          id:localStorage.getItem('id'),
+          email:localStorage.getItem('email'),
+          phoneNumber:localStorage.getItem('phoneNumber'),
+          selfIntroduction:localStorage.getItem('selfIntroduction')
+        }
+      }).then(res => {
+        console.log('dd');
+        if(res.data.code === "0"){
+          this.successMessage = '个人资料修改成功';
+          this.errorMessage = '';
+          localStorage.setItem('id',this.edit.e_id);
+          localStorage.setItem('email',this.edit.e_email);
+          localStorage.setItem('phoneNumber',this.edit.e_phoneNumber);
+          localStorage.setItem('selfIntroduction',this.edit.e_selfIntroduction);
+          console.log('sss');
+        }else {
+          console.log("error")
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    },
     go(route) {
       this.$router.push(route);
     },
@@ -118,7 +241,7 @@ export default {
         }).catch(error => {
           console.error('Error loading course materials:', error);
         });
-      //加载assignments
+        //加载assignments
         await this.$axios.get('/course/assignments', {
           params: {
             courseId: course.id
@@ -133,7 +256,7 @@ export default {
               localStorage.setItem('assignmentdescription'+course.title+i,res.data.data[i].assignmentDescription);
               localStorage.setItem('assignmentddl'+course.title+i,res.data.data[i].assignmentDeadline);
               this.ddls.push({
-                  date : res.data.data[i].assignmentDeadline,
+                date : res.data.data[i].assignmentDeadline,
                 title : course.title+"   "+res.data.data[i].assignmentTitle,
               })
               this.currentddl.push({
@@ -325,9 +448,9 @@ el-button{
 .rili2 {
   flex-basis: 300px; /* 日历的宽度 */
 }
- .user-profile {
-   text-align: center; /* 居中用户信息 */
- }
+.user-profile {
+  text-align: center; /* 居中用户信息 */
+}
 
 .avatar {
   width: 80px; /* 头像大小 */
@@ -387,4 +510,3 @@ el-button{
 }
 
 </style>
-
