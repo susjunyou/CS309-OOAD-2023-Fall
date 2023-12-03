e85aa939428c78b0db4108b309465c606af6071b
susjunyou <12110911@mail.sustech.edu.cn>
2023-12-03 13:40:39 +0800

    add entity joinTeamInfo

---
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/entity/joinTeamInfo.java
@@ -0,0 +1,20 @@
+package com.example.ooad_project_backend.entity;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data // 自动生成get、set方法
+@EqualsAndHashCode(callSuper = false)// 自动生成equals和 hashcode方法
+@Accessors(chain = true)    // 允许链式访问，例如：myObject.setField1("value1").setField2(42)。
+public class joinTeamInfo {
+
+    Integer Id;
+
+    Integer teamId;
+
+    Integer studentId;
+
+    Integer projectId;
+}
