8bd0af543f007b53849bc06a3ff5ecc83ad6c3d9
susjunyou <12110911@mail.sustech.edu.cn>
2023-12-11 16:03:23 +0800

    fix register

--- backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/entity/UserInfo.java
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/entity/UserInfo.java
@@ -1,5 +1,7 @@
 package com.example.ooad_project_backend.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.example.ooad_project_backend.enums.UserType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,6 +19,7 @@ public class UserInfo implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     private String email;
