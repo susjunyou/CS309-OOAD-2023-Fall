a0c8c9675378d67f7d3f4206faf5124df72a199d
DiDicpR <106336303+DiDicpR@users.noreply.github.com>
2023-12-18 11:39:11 +0800

    enable Project to upload file

--- backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/mapper/ProjectMapper.java
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/mapper/ProjectMapper.java
@@ -12,16 +12,22 @@ import java.util.List;
 @Mapper
 public interface ProjectMapper extends BaseMapper<ProjectInfo> {
 
-        @Insert("insert into project(project_title, project_description, project_deadline, project_status, max_score, proportion, releaser, releaser_type, course_id, project_start_date, max_people_in_team) values(#{projectTitle}, #{projectDescription}, #{projectDeadline}, #{projectStatus}, #{maxScore}, #{proportion}, #{releaser}, #{releaserType}, #{courseId}, #{projectStartDate}, #{maxPeopleInTeam})")
+        @Insert("insert into project(project_title, project_description, project_deadline, project_status, max_score, proportion, releaser, releaser_type, course_id, project_start_date, max_people_in_team, file_id) values(#{projectTitle}, #{projectDescription}, #{projectDeadline}, #{projectStatus}, #{maxScore}, #{proportion}, #{releaser}, #{releaserType}, #{courseId}, #{projectStartDate}, #{maxPeopleInTeam}, #{fileId})")
         boolean addProject(@Param("projectTitle") String projectTitle, @Param("projectDescription") String projectDescription, @Param("projectDeadline") Date projectDeadline, @Param("projectStatus") String projectStatus,
-                        @Param("maxScore") Integer maxScore, @Param("proportion") Double proportion, @Param("releaser") String releaser, @Param("releaserType") UserType releaserType, @Param("courseId") Integer courseId, @Param("projectStartDate") Date projectStartDate, @Param("maxPeopleInTeam") Integer maxPeopleInTeam);
+                        @Param("maxScore") Integer maxScore, @Param("proportion") Double proportion, @Param("releaser") String releaser, @Param("releaserType") UserType releaserType, @Param("courseId") Integer courseId, @Param("projectStartDate") Date projectStartDate, @Param("maxPeopleInTeam") Integer maxPeopleInTeam, @Param("fileId") Integer fileId);
 
         @Delete("delete from project where id = #{projectId}")
         boolean deleteProject(@Param("projectId") Integer projectId);
 
-        @Update("update project set project_title = #{projectTitle}, project_description = #{projectDescription}, project_deadline = #{projectDeadline}, project_status = #{projectStatus}, max_score = #{maxScore}, proportion = #{proportion}, releaser = #{releaser}, releaser_type = #{releaserType}, course_id = #{courseId}, project_start_date = #{projectStartDate}, max_people_in_team = #{maxPeopleInTeam} where id = #{projectId}")
+        @Delete("delete from file where id = #{fileId}")
+        boolean deleteFileById(@Param("fileId") Integer fileId);
+
+        @Select("select * from project where id = #{projectId}")
+        ProjectInfo getProjectByProjectId(@Param("projectId") Integer projectId);
+
+        @Update("update project set project_title = #{projectTitle}, project_description = #{projectDescription}, project_deadline = #{projectDeadline}, project_status = #{projectStatus}, max_score = #{maxScore}, proportion = #{proportion}, releaser = #{releaser}, releaser_type = #{releaserType}, course_id = #{courseId}, project_start_date = #{projectStartDate}, max_people_in_team = #{maxPeopleInTeam},file_id = #{fileId} where id = #{projectId}")
         boolean updateProject(@Param("projectId") Integer projectId, @Param("projectTitle") String projectTitle, @Param("projectDescription") String projectDescription, @Param("projectDeadline") Date projectDeadline, @Param("projectStatus") String projectStatus,
-                            @Param("maxScore") Integer maxScore, @Param("proportion") Double proportion, @Param("releaser") String releaser, @Param("releaserType") UserType releaserType, @Param("courseId") Integer courseId, @Param("projectStartDate") Date projectStartDate, @Param("maxPeopleInTeam") Integer maxPeopleInTeam);
+                            @Param("maxScore") Integer maxScore, @Param("proportion") Double proportion, @Param("releaser") String releaser, @Param("releaserType") UserType releaserType, @Param("courseId") Integer courseId, @Param("projectStartDate") Date projectStartDate, @Param("maxPeopleInTeam") Integer maxPeopleInTeam, @Param("fileId") Integer fileId);
 
         @Select("select * from project where course_id = #{courseId}")
         List<ProjectInfo> getProjectByCourseId(@Param("courseId") Integer courseId);
