c23f5cb542d2cd1409359d0f19f03371e411a5c7
susjunyou <12110911@mail.sustech.edu.cn>
2023-12-18 14:36:51 +0800

    fix projectController

--- backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/controller/ProjectController.java
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/controller/ProjectController.java
@@ -2,10 +2,13 @@ package com.example.ooad_project_backend.controller;
 
 
 import com.example.ooad_project_backend.common.Result;
+import com.example.ooad_project_backend.entity.FileInfo;
 import com.example.ooad_project_backend.enums.UserType;
+import com.example.ooad_project_backend.service.FileService;
 import com.example.ooad_project_backend.service.ProjectInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Date;
 import java.util.List;
@@ -19,16 +22,32 @@ public class ProjectController {
     @Autowired
     private ProjectInfoService projectInfoService;
 
+    @Autowired
+    private FileService fileService;
+
     @PostMapping("/addProject")
     public Result addProject(String projectTitle, String projectDescription, Date projectDeadline, String projectStatus,
-                             Integer maxScore, Double proportion, String releaser, UserType releaserType,Date projectStartDate,Integer maxPeopleInTeam, Integer courseId) {
+                             Integer maxScore, Double proportion, Integer releaser, UserType releaserType, Date projectStartDate, Integer maxPeopleInTeam, Integer courseId,  MultipartFile file) {
+
+        FileInfo fileInfo = new FileInfo();
+        try {
+            if (file != null) {
+                fileInfo.setFileName(file.getOriginalFilename());
+                fileInfo.setFileType(file.getContentType());
+                fileInfo.setFileData(file.getBytes());
+                fileInfo = fileService.insertFile(fileInfo);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         boolean flag = projectInfoService.addProject(projectTitle, projectDescription, projectDeadline, projectStatus,
-                maxScore, proportion, releaser, releaserType, courseId,projectStartDate,maxPeopleInTeam);
+                maxScore, proportion, releaser, releaserType, courseId,projectStartDate,maxPeopleInTeam,fileInfo.getId());
         if (flag) {
             return Result.success();
         } else {
             return Result.error("1", "添加失败");
         }
+
     }
 
     @DeleteMapping("/deleteProject")
@@ -43,9 +62,21 @@ public class ProjectController {
 
     @GetMapping("/updateProject")
     public Result updateProject(Integer projectId, String projectTitle, String projectDescription, Date projectDeadline, String projectStatus,
-                                Integer maxScore, Double proportion, String releaser, UserType releaserType,Date projectStartDate,Integer maxPeopleInTeam, Integer courseId) {
+                                Integer maxScore, Double proportion, Integer releaser, UserType releaserType,Date projectStartDate,Integer maxPeopleInTeam, Integer courseId, MultipartFile file) {
+
+        FileInfo fileInfo = new FileInfo();
+        try {
+            if (file != null) {
+                fileInfo.setFileName(file.getOriginalFilename());
+                fileInfo.setFileType(file.getContentType());
+                fileInfo.setFileData(file.getBytes());
+                fileInfo = fileService.insertFile(fileInfo);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         boolean flag = projectInfoService.updateProject(projectId, projectTitle, projectDescription, projectDeadline, projectStatus,
-                maxScore, proportion, releaser, releaserType, courseId,projectStartDate,maxPeopleInTeam);
+                maxScore, proportion, releaser, releaserType, courseId,projectStartDate,maxPeopleInTeam,fileInfo.getId());
         if (flag) {
             return Result.success();
         } else {
--- backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/service/ProjectInfoService.java
+++ backend/OOAD_Project_Backend/src/main/java/com/example/ooad_project_backend/service/ProjectInfoService.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface ProjectInfoService {
     boolean addProject(String projectTitle, String projectDescription, Date projectDeadline, String projectStatus,
-                       Integer maxScore, Double proportion, String releaser, UserType releaserType, Integer courseId,Date projectStartDate,Integer maxPeopleInTeam,Integer fileId);
+                       Integer maxScore, Double proportion, Integer releaser, UserType releaserType, Integer courseId,Date projectStartDate,Integer maxPeopleInTeam,Integer fileId);
     boolean deleteProject(Integer projectId);
     boolean updateProject(Integer projectId, String projectTitle, String projectDescription, Date projectDeadline, String projectStatus,
-                          Integer maxScore, Double proportion, String releaser, UserType releaserType, Integer courseId, Date projectStartDate, Integer maxPeopleInTeam,Integer fileId);
+                          Integer maxScore, Double proportion, Integer releaser, UserType releaserType, Integer courseId, Date projectStartDate, Integer maxPeopleInTeam,Integer fileId);
     List<ProjectInfo> getProjectByCourseId(Integer courseId);
 
 }
